1ab6684a948ecfed0e39b109d053710eeafeb6d7
Kousik Dasari <kousik@apxor.com>
2026-01-12 13:38:06 +0530

    inital test

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "ApxorSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ApxorSDK",
-            targets: ["ApxorSDK"]),
+            targets: ["ApxorSDK"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ApxorSDK"),
-
+        .binaryTarget(
+            name: "ApxorSDK",
+            url: "https://repo.apxor.com/artifactory/libs-release-ios/core/21044/ApxoriOSSDK-Core-21044.zip",
+            checksum: "299e4ba6c48d5159b4bd6db1193f1dac4a9fdbc43248b100a641adc82c478570"
+        )
     ]
 )
